c580c15c534d9f35769c54d6fcdb41f73dbfe6c0
jiahuizheng <ninhuijia@gmail.com>
2024-05-19 22:39:44 +0100

    set up Radix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
+import { Button } from '@radix-ui/themes';
 
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <div><Button>New Issue</Button></div>
   )
 }
