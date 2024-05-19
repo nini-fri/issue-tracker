100c2e6411205715af7a80c2455b2887cf1a2ab0
jiahuizheng <ninhuijia@gmail.com>
2024-05-19 23:03:18 +0100

    organize import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import NavBar from './NavBar'
-import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import NavBar from './NavBar';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
