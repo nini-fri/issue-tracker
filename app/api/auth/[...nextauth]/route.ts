b1210c4a38458f8a25faf1df4be65c27d5b73612
jiahuizheng <ninhuijia@gmail.com>
2024-05-22 17:19:52 +0100

    secure application

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,10 @@
 import NextAuth from "next-auth"
+import authOptions from "@/app/auth/authOptions";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/prisma/client";
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-    })
-  ],
-  session: {
-    strategy: 'jwt'
-  }
-});
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST }
--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,13 +1,19 @@
-import { issueSchema } from "@/app/validationSchema";
-import prisma from "@/prisma/client";
-import { NextRequest, NextResponse } from "next/server";
+import authOptions from '@/app/auth/authOptions';
+import { issueSchema } from '@/app/validationSchema';
+import prisma from '@/prisma/client';
+import { getServerSession } from 'next-auth';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function PATCH(
   request: NextRequest,
-  { params }: { params: { id: string }}) {
+  { params }: { params: { id: string } }
+) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
   if (!validation.success)
+
     return NextResponse.json(validation.error.format(), { status: 400 });
 
   const issue = await prisma.issue.findUnique({
@@ -29,7 +35,10 @@ export async function PATCH(
 
 export async function DELETE(
   request: NextRequest,
-  { params }: { params: { id: string }}) {
+  { params }: { params: { id: string } }
+) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) }
