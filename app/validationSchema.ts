159323bf2a4ea7e9b8b11261ad03e1aabe7d6b03
jiahuizheng <ninhuijia@gmail.com>
2024-05-23 15:42:26 +0100

    Develope API to assign issues

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -4,5 +4,20 @@ export const issueSchema = z.object({
   title: z.string().min(1, 'Title is required.').max(255),
   description:
   z.string({ required_error: 'Description is required.' })
-  .min(1, 'Description is required.'),
+  .min(1, 'Description is required.').max(65535),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, 'Title is required.').max(255).optional(),
+  description:
+  z.string({ required_error: 'Description is required.' })
+  .min(1, 'Description is required.')
+  .max(65535)
+  .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required.")
+    .max(255)
+    .optional()
+    .nullable(),
 });
