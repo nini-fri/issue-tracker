66dd8cb37ce1851baf0cd2e541fa1c472cfcdbb1
jiahuizheng <ninhuijia@gmail.com>
2024-05-24 12:18:07 +0100

    build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
 import { Button } from '@radix-ui/themes';
+import Pagination from './components/Pagination';
 
 export default function Home() {
   return (
-    <div><Button>New Issue</Button></div>
+    <Pagination itemCount={100} pageSize={10} currentPage={1} />
   )
 }
