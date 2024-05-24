637399816cc34dd8a2b9474bce71512dffa1e441
jiahuizheng <ninhuijia@gmail.com>
2024-05-24 11:24:15 +0100

    build column sorting function

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -4,25 +4,53 @@ import IssueActions from "./IssueActions";
 import delay from "delay";
 import { Table, Button } from "@radix-ui/themes";
 import { IssueStatusBadge, Link } from "@/app/components";
-import { Status } from "@prisma/client";
+import { Issue, Status } from "@prisma/client";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
+import NextLink from 'next/link';
 
 interface Props {
-  searchParams: { status: Status };
+  searchParams: { status: Status, orderBy: keyof Issue };
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
+  const columns: {
+    label: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
+    { label: "Issue", value: "title" },
+    {
+      label: "Status",
+      value: "status",
+      className: "hidden md:table-cell",
+    },
+    {
+      label: "Created",
+      value: "createdAt",
+      className: "hidden md:table-cell",
+    },
+  ];
+
+  //await delay(2000);
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
-    ? searchParams.status
-    : undefined;
+  ? searchParams.status
+  : undefined;
+
+  const orderBy = columns
+  .map(column => column.value)
+  .includes(searchParams.orderBy)
+  ? { [searchParams.orderBy]: 'asc' }
+  : undefined;
+
 
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy
   });
 
-  //await delay(2000);
 
   return (
     <div>
@@ -31,13 +59,14 @@ const IssuesPage = async ({ searchParams }: Props) => {
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+          {columns.map((column) => (
+              <Table.ColumnHeaderCell key={column.value}>
+                <NextLink href={{
+                  query: { ...searchParams, orderBy: column.value }
+                }}>{column.label}</NextLink>
+                {column.value === searchParams.orderBy && <ArrowUpIcon className="inline"/>}
+              </Table.ColumnHeaderCell>
+          ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
