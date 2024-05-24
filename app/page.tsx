3dea4f8bf51d7919c5efe605a0dc94d855334a47
jiahuizheng <ninhuijia@gmail.com>
2024-05-24 12:24:51 +0100

    implement pagination funciton

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from 'next/image'
 import { Button } from '@radix-ui/themes';
 import Pagination from './components/Pagination';
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
-    <Pagination itemCount={100} pageSize={10} currentPage={1} />
+    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)}  />
   )
 }
