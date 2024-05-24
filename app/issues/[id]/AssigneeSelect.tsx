5cd013e476b395355d0243dc0ac4dde67f0657d8
jiahuizheng <ninhuijia@gmail.com>
2024-05-24 10:30:51 +0100

    show toast notification

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,9 +1,10 @@
-'use client';
-import { Skeleton } from '@/app/components';
-import { Issue, User } from '@prisma/client';
-import { Select } from '@radix-ui/themes';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
+"use client";
+import { Skeleton } from "@/app/components";
+import { Issue, User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
@@ -12,41 +13,47 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     isLoading,
   } = useQuery<User[]>({
     queryKey: ["users"],
-    queryFn: () =>
-      axios.get("/api/users").then((res) => res.data),
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
     staleTime: 60 * 1000,
-    retry: 3
-  })
+    retry: 3,
+  });
 
-  if (isLoading) return <Skeleton/>
+  if (isLoading) return <Skeleton />;
   if (error) return null;
   return (
-    <Select.Root
-    defaultValue={issue.assignedToUserId || "unassigned"}
-    onValueChange={(userId) => {
-    // Check if the value is "unassigned" and convert it to null for the backend
-    const valueToSend = userId === "unassigned" ? null : userId;
-    axios.patch("/api/issues/" + issue.id, {
-      assignedToUserId: valueToSend,
-    })
-    .then(response => console.log("Success:", response))
-    .catch(error => console.error("Patch request failed:", error));
-  }}
->
-    <Select.Trigger placeholder="Assign..." />
-      <Select.Content>
-      <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="unassigned">Unassigned</Select.Item>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
-  )
-}
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || "unassigned"}
+        onValueChange={(userId) => {
+          // Check if the value is "unassigned" and convert it to null for the backend
+          const valueToSend = userId === "unassigned" ? null : userId;
+          axios
+            .patch("/api/issues/" + issue.id, {
+              assignedToUserId: valueToSend,
+            })
+            .then((response) => console.log("Success:", response))
+            //.catch((error) => console.error("Patch request failed:", error));
+            .catch(() => {
+              toast.error('Changes could not be saved.');
+            });
+        }}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="unassigned">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
+  );
+};
 
-export default AssigneeSelect
+export default AssigneeSelect;
